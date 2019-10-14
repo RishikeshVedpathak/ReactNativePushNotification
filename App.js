ee26ce587913d9ea6d0a525187a42cbde8ee1066
RishikeshVedpathak <rishikeshvedpathak@gmail.com>
2019-10-14 12:48:11 +0530

    Push notification integration

--- App.js
+++ App.js
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Notifications } from 'expo';
+import * as Permissions from 'expo-permissions';
 
 export default function App() {
+  useEffect(() => {
+    this.getPushNotificationPermissions();
+  });
+
+  getPushNotificationPermissions = async () => {
+    const { status: existingStatus } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
+    let finalStatus = existingStatus;
+
+    // only ask if permissions have not already been determined, because
+    // iOS won't necessarily prompt the user a second time.
+    if (existingStatus !== 'granted') {
+      // Android remote notification permissions are granted during the app
+      // install, so this will only ask on iOS
+      const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+      finalStatus = status;
+    }
+
+    // Stop here if the user did not grant permissions
+    if (finalStatus !== 'granted') {
+      return;
+    }
+    console.log(finalStatus)
+
+    // Get the token that uniquely identifies this device
+    console.log("Notification Token: ", await Notifications.getExpoPushTokenAsync());
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>React Native Push Notification</Text>
     </View>
   );
 }
